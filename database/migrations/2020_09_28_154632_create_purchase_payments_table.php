fdc782a9a2473cb9eb9c5d529628fe9649b33906
Denis Joel Torres Farro <djoe_torres@hotmail.com>
2020-10-06 11:48:50 -0500

    tiket nota

--- database/migrations/2020_09_28_154632_create_purchase_payments_table.php
+++ database/migrations/2020_09_28_154632_create_purchase_payments_table.php
@@ -34,6 +34,8 @@ class CreatePurchasePaymentsTable extends Migration
      */
     public function down()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('purchase_payments');
+        Schema::enableForeignKeyConstraints();
     }
 }
