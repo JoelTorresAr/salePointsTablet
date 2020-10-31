8eee5a492e6764544e6a787c8bc97b146dbafc65
Denis Joel Torres Farro <djoe_torres@hotmail.com>
2020-10-31 16:54:10 -0500

    last commit

--- Ejecutar servidor PROD - copia.bat
+++
@@ -1,6 +0,0 @@
-php artisan config:clear
-php artisan cache:clear
-php artisan config:cache
-php artisan route:cache
-php artisan optimize
-php artisan serve
--- Ejecutar servidor PROD.bat
+++
@@ -1,6 +0,0 @@
-php artisan config:clear
-php artisan cache:clear
-php artisan config:cache
-php artisan route:cache
-php artisan optimize
-php artisan serve --host=192.168.0.94 --port=8080
